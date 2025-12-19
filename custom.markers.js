d1d0bc923f0b1d3590d7488858192c8e50745d7f
IDOL <dylanmalave2007@gmail.com>
2025-12-18 19:19:48 -0500

    Update custom.markers.js

--- custom.markers.js
+++ custom.markers.js
@@ -42,42 +42,8 @@ UnminedCustomMarkers = {
 
     markers: [
 
-        // Example 1: Simple marker
+            // France
         {
-            x: -200,
-            z: -200,
-            image: "custom.pin.png",
-            imageAnchor: [0.5, 1],
-            imageScale: 0.5,
-        },
-
-        // Example 2: Marker with text
-        {
-            x: 0,
-            z: 0,
-            image: "custom.pin.png",
-            imageAnchor: [0.5, 1],
-            imageScale: 0.5,
-            text: "Marker with text",
-            textColor: "red", 
-            offsetX: 0,
-            offsetY: 20,
-            font: "bold 20px Calibri,sans serif",
-        },
-
-        // Example 3: Text only
-        {
-            x: 200,
-            z: 200,
-            text: "Text only", 
-            textColor: "yellow", 
-            offsetX: 0,
-            offsetY: 0, 
-            font: "bold 50px Calibri,sans serif",
-        },
-
-      // France
-                {
             x: 1994,
             z: 4192,
             image: "images/france.jpg",
